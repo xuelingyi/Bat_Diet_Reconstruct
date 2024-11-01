94a83cea49ca9d4ccd7eaee1c965aeb7c95a9c97
Xueling Yi <willyxl3@163.com>
2024-11-01 17:23:03 +0100

    Add files via upload

---
+++ check_ESS_PSRF_modified_ppca.R
@@ -0,0 +1,154 @@
+#!/usr/bin/env Rscript
+
+## NB! run this in the dir where the results are saved
+
+# This script checks if a model that was fitted with MCMCglmm was 
+# executed for a sufficient number of MCMC generations. It does so
+# by examining the effective sample size and potential scale reduction 
+# factor of each model parameter.
+
+# The script needs to be run from the command line, with the user providing the directory where the model outputs (*.Rda files) are found, and the number of traits included, e.g.:
+
+# Rscript check_ESS_PSRF.R ../Results/MCMCglmm_fits/ 6
+
+library(MCMCglmm, lib="~/R_4.4")
+
+#############################
+# F  U  N  C  T  I  O  N  S #
+#############################
+
+# This function checks if the effective sample size (ESS) of each model 
+# parameter per chain is at least 400. If not, this means that the 
+# parameter space was not adequately explored.
+check_ESS <- function(model_fits, n.traits)
+{
+  
+  # Initialise a vector to store the ESS for each model parameter per 
+  # chain.
+  all_ESS_vals <- c()
+  
+  # Store ESS values in the vector.
+  for ( i in 1:length(model_fits) )
+  {
+    all_ESS_vals <- c(
+      all_ESS_vals,
+      effectiveSize(model_fits[[i]]$Sol[,1:n.traits]),
+      effectiveSize(model_fits[[i]]$VCV)
+    )
+  }
+  
+  # Remove non-positive ESS values (which can occur if a parameter 
+  # is constant).
+  all_ESS_vals <- all_ESS_vals[all_ESS_vals > 0]
+  
+  # Then, check if any parameter has an ESS value below 400.
+  low_ESS <- which(all_ESS_vals < 400)
+  
+  if ( length(low_ESS) > 0 )
+  {
+    stop('PROBLEM! The ESS is not big enough!')
+  } else
+  {
+    cat('Everything OK!\n', sep = '')
+  }
+}
+
+# This function checks if the potential scale reduction factor (PSRF; 
+# Gelman & Rubin, Stat. Sci., 1992) value of each model parameter is 
+# below 1.1. If not, this means that the chains did not converge on 
+# statistically indistinguishable posterior distributions.
+check_PSRF <- function(model_fits, n.traits)
+{
+  
+  # Initialise lists to store the model parameter values per chain. 
+  ## no CP in continuous-only analyses (ppca)
+  Sols <- 'mcmc.list('
+  VCVs <- 'mcmc.list('
+  
+  # Store the values in the lists.
+  for ( i in 1:length(model_fits) ) {
+    Sols <- paste(
+      Sols, 'model_fits[[', i, ']]$Sol[,1:', n.traits, '], ', sep = ''
+    )
+    VCVs <- paste(
+      VCVs, 'model_fits[[', i, ']]$VCV, ', sep = ''
+    )
+  }
+  
+  Sols <- sub(', $', ')', Sols)
+  VCVs <- sub(', $', ')', VCVs)
+  
+  # Calculate the PSRF for each parameter.
+  psrf_vals <- c(
+    gelman.diag(
+      eval(parse(text = Sols)), multivariate = FALSE
+    )$psrf[,1],
+    gelman.diag(
+      eval(parse(text = VCVs)), multivariate = FALSE
+    )$psrf[,1]
+  )
+  
+  # Remove NA values (which can occur if a parameter is constant).
+  psrf_vals <- psrf_vals[!is.na(psrf_vals)]
+  
+  # Then, check if any parameter has a PSRF value of 1.1 or higher.
+  high_psrf <- which(psrf_vals >= 1.1)
+  
+  if ( length(high_psrf) > 0 )
+  {
+    print("parameters having PSRF >= 1.1:")
+    print(psrf_vals[high_psrf])
+    stop("PROBLEM! The chains have not converged!")
+  } else
+  {
+    cat('Everything OK!\n', sep = '')
+  }
+}
+
+plot_trace_density = function(chain_id, result.dir, ...){
+  pdf(paste0(result.dir, "Trace_Density_chain", chain_id, ".pdf"), height = 3, width = 6)
+  
+  for(j in diets){
+    print(plot(fit$VCV[,grep(paste0(paste0("trait", j), ":", paste0("trait", j, ".vertlife_name")), 
+                       colnames(fit$VCV))]) + title(main=j))
+  }
+  dev.off()
+}
+
+
+############################
+# M  A  I  N    C  O  D  E #
+############################
+
+# Read the working directory provided by the user as a command line argument.
+args <- commandArgs(TRUE)
+n.traits = as.numeric(args[1])
+plot.all.chains = as.character(args[2])
+
+working_dir = "./"
+# Look for model fit files in the working directory.
+Rda_files_in_working_dir <- list.files(
+  path = working_dir, pattern = '^\\d+\\.Rda$'
+)
+
+current_model_fits <- list()
+for ( i in 1:length(Rda_files_in_working_dir) ) {
+  
+  # ... load the corresponding .Rda files...
+  cat('Now loading ', Rda_files_in_working_dir[i], '...\n', sep ='')
+  load(paste(working_dir, '/', Rda_files_in_working_dir[i], sep = ''))
+  current_model_fits[[i]] <- fit
+
+  diets = gsub("trait", "", colnames(fit$Sol)[1:n.traits])
+
+  if(plot.all.chains == "T"){
+    plot_trace_density(i, working_dir)
+  }
+}
+
+# ... and check the ESS and PSRF.
+cat('Now checking the ESS... ', sep = '')
+check_ESS(current_model_fits, n.traits)
+
+cat('Now checking the PSRF... ', sep = '')
+check_PSRF(current_model_fits, n.traits)
---
+++ vertlife_MCMC_pca.R
@@ -0,0 +1,107 @@
+#!/usr/bin/env Rscript
+
+library(ape)
+library(MCMCglmm, lib="~/R_4.4")
+library(phytools, lib="~/R_4.4")
+
+## !NOTE: run this script in the dir sif179/trees/tree_id
+
+# Read the user-provided command line arguments.
+args <- commandArgs(TRUE)
+batch=as.numeric(args[1])
+batch_tree_index=as.numeric(args[2])
+chain_id <- as.numeric(args[3])
+my.nitt <- as.numeric(args[4])
+# 2000000 default
+my.thin <- as.numeric(args[5])
+#50 default
+diet.coding <- args[7]
+## raw, conserve
+
+out.dir = "./"
+my.burnin = my.nitt*0.1
+# 10% burin
+
+load(paste0("../../inputs/sif176_ppca_topo11_index_", batch_tree_index, ".RData"))
+# tree, scores, diet.in, diet.states, mono.nodes
+n.edge = length(tree$edge)/2
+
+## priors
+my.prior = list(
+  ## prior for the random effect
+  G = list(
+    G1 = list(
+      V=diag(length(diet.in)), nu=length(diet.in),
+      alpha.mu=rep(0, length(diet.in)), alpha.V=diag(rep(1000, length(diet.in)))
+    )
+  ),
+  ## prior for the residual
+  R = list(V = diag(length(diet.in)), nu=length(diet.in))
+)
+
+## use scale only when the tree is ultrametric 
+if(is.ultrametric(tree)){
+  my.scale=TRUE
+  print("scale trees")
+  cat("\n")
+} else {
+  my.scale=FALSE
+  print("not ultrametric tree; do not scale")
+  cat("\n")
+}
+
+# Set the seed and fit the model.
+set.seed(chain_id)
+
+## use ppca scores pc1 and pc2, continuous trait
+# fit a model with MCMCglmm to estimate the correlation structure between pairs of diet items, accounting for phylogeny.
+fit <- MCMCglmm(
+  # Define the response variables, apply any needed transformations, and specify a distinct intercept per response.
+  cbind(PC1, PC2) ~ trait - 1,
+  
+  # Specify a phylogenetic random effect on each intercept.
+  # us: estimate both variance and covariance
+  random =~ us(trait):vertlife_name,
+  
+  # Set the distribution for each response variable (gaussian or threshold)
+  family = rep("gaussian", length(diet.in)),
+  
+  # only scale is ultrametric tree
+  scale=my.scale,
+  
+  # Integrate the phylogenetic variance-covariance matrix into the model; scale=F as the tree is not ultrametric
+  ginverse = list(vertlife_name=inverseA(tree, nodes = 'ALL', scale = my.scale)$Ainv),
+  
+  # Set relatively uninformative priors.
+  prior = my.prior,
+  
+  # Set the data frame with all the needed data.
+  data = scores,
+  
+  # formula for residual covariance structure. This has to be set up so that each data point is associated with a unique residual. For example a multi-response model might have the R-structure defined by ~us(trait):units
+  # covariances are useless here as variances are fixed to 1 (see prior).
+  rcov =~ us(trait):units,
+  
+  # Set the (total) number of iterations, the burn-in, and the sampling frequency.
+  nitt = my.nitt,
+  burnin = my.burnin,
+  thin = my.thin,
+  verbose = TRUE,
+  
+  DIC = TRUE,
+  
+  # Store the posterior distributions of random effects and latent variables.
+  pr = TRUE,
+  pl = TRUE,
+  
+  # Force the threshold liabilities to range from -7 to 7 to facilitate
+  # their estimation.
+  trunc = TRUE
+)
+
+# Wait for 10 seconds after fitting has finished (just in case), create an output directory if missing, and save the resulting fit to an .Rda file.
+Sys.sleep(10)
+
+save(fit, file = paste(out.dir, chain_id, '.Rda', sep = ''))
+print(paste0("output file saved: ", out.dir, chain_id, ".Rda"))
+
