2acc53b20642811aaa85f48a683ed7d8bab14e80
Xueling Yi <willyxl3@163.com>
2024-11-25 13:33:18 +0100

    Update aggregate.R

--- aggregate.R
+++ aggregate.R
@@ -1,6 +1,21 @@
 #!/usr/bin/env Rscript
 library(ape)
-## run this in sif179
+
+########################################################################
+## This script aggregates results across trees using the estimates saved in summary.RData (i.e., only tips and the labeled nodes of interest). 
+## run this script in the same dir as the folders trees_conserve or trees_raw
+# batch=1
+# rdata=sif176_100tree_batch1_diet5
+# id_start=1
+# id_end=100
+
+# for code in raw conserve
+# do
+# Rscript aggregate.R ${batch} ${rdata} ${id_start} ${id_end} ${code}
+# done
+#######################################################################
+
+
 args <- commandArgs(TRUE)
 batch = as.numeric(args[1])
 rdata = args[2]
@@ -9,6 +24,8 @@ id_end = as.numeric(args[4])
 diet.code=args[5]
 
 trees = read.table(paste0("tree100_batch", batch, ".txt"))
+## or tree23_batch2 
+
 load(paste0(rdata, ".RData"))
 ## tree_100_sub, sif, diet.in, diet.states
 
@@ -37,5 +54,5 @@ for(id in (id_start+1):id_end){
 
 setwd("../")
 
-save(list = grep("aggregate", ls(), value=T),
-     file=paste0("mcmc_aggregate_diet", length(diet.in), "_", diet.code, "_", id_start, "_", id_end, ".RData"))
+save(list = grep("aggregate", ls(), value=T), 
+     file=paste0("mcmc_aggregate_diet6_", diet.code, "_", id_start, "_", id_end, ".RData"))
